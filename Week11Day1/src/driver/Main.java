4c6150771decc59afebcb02e09737f94b3d047ba
blade <yiming.liao@cuw.edu>
2024-11-11 10:58:38 -0600

    test compare to

--- Week11Day1/src/driver/Main.java
+++ Week11Day1/src/driver/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Doubly Linked List");
-
+		Node<Integer> a = new Node();
+		a.Payload = 5;
+		Node<Integer> b  = new Node();
+		b.Payload = 7;
+		System.out.println(a.compareTo(b));
 	}
 
 }
